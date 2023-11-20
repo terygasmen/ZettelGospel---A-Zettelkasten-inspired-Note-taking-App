f21241c512fd60edb1240868217a321622619197
terygasmen <terygasmen@gmail.com>
2023-11-20 18:36:27 +0800

    changed url

--- backend/server.js
+++ backend/server.js
@@ -89,8 +89,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || '0.0.0.0';
-
 app.listen(PORT, HOST, function() {
-  console.log(`App listening at http://${HOST}:${PORT}`);
+  console.log(`App listening at http://localhost:${PORT}`);
 });
